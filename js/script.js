ece34240a3623a8e6838f36041167668f07beef6
Aku Ocampo <akuocampo@gmail.com>
2022-11-20 17:07:58 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let pais = "Rusia";
+let continente = "Asia";
+let antiguedad = "2019";
+let pais_y_continente = pais + " " + continente;
+
+pais = "Argentina";
+continente = "Latinoamerica";
+
+//Este muestra por consola los valores que hemos definido, en este caso "Argentina, Latinoamerica, 2019"
+console.log(pais, continente, antiguedad);
+//Acá hace la alerta mostrando en este caso "Rusia, Asia" porque muestra la variable pais_y_continente
+alert(pais_y_continente);
